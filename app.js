81b1c7a3e77c8532a35fc0bf161cf7e5998fe447
FarahSyed <farahsyed44@gmail.com>
2022-08-07 23:53:07 +0500

    Added mad-lib replacement variables to story

---
+++ app.js
@@ -0,0 +1,37 @@
+"use strict";
+
+let userInputs = [];
+
+let originalStory = `Hi! My name is ${userInputs[0]} and I have a secret to share
+    with you. I'm a normal child by day, and a ${userInputs[1]} by night.
+    Only you and my best friend ${userInputs[2]} knows my secret. You may be wondering
+    how this happened? Well, one night I was ${userInputs[3]}
+    at home, and then BOOM! The lights went out and ${userInputs[4]} showed up.
+    He/she said in a booming voice, because your favorite color is ${userInputs[5]}
+    you have been chosen to be a ${userInputs[1]} My mission is to save the people of
+    ${userInputs[6]} by doing my favorite things: ${userInputs[7]},
+    ${userInputs[8]} and ${userInputs[9]}.
+    This may sound easy, but it is no walk in the park. It requires hard work and
+    ${userInputs[10]}. My weakness is eating ${userInputs[11]}.
+    They are gross!! Keep that away from me! I save the world every night. But when I wake up in the
+    morning, I go back to my normal life at ${userInputs[12]} School!`;
+
+
+
+
+
+
+
+// Hi! My name is Sam and I have a secret to share
+// with you. I'm a normal child by day, and a Superman by night.
+// Only you and my best friend Jack knows my secret. You may be wondering
+// how this happened? Well, one night I was doing my homework
+// at home, and then BOOM! The lights went out and Filly the fairy showed up.
+// He/she said in a booming voice, because your favorite color is blue
+// you have been chosen to be a Superman My mission is to save the people of
+// New York City by doing my favorite things: flying,
+// running and jumping.
+// This may sound easy, but it is no walk in the park. It requires hard work and
+// dedication. My weakness is eating noodles.
+// They are gross!! Keep that away from me! I save the world every night. But when I wake up in the
+// morning, I go back to my normal life at New York City School!
