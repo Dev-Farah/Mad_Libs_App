d9bdb294e2b5236c3066ca7dd43b3b4d0423c1f2
FarahSyed <farahsyed44@gmail.com>
2022-08-13 15:42:12 +0500

    Final Polish

--- app.js
+++ app.js
@@ -2,20 +2,6 @@
 
 let userInputs = [];
 
-let originalStory = `Hi! My name is ${userInputs[0]} and I have a secret to share
-    with you. I'm a normal child by day, and a ${userInputs[1]} by night.
-    Only you and my best friend ${userInputs[2]} knows my secret. You may be wondering
-    how this happened? Well, one night I was ${userInputs[3]}
-    at home, and then BOOM! The lights went out and ${userInputs[4]} showed up.
-    He/she said in a booming voice, because your favorite color is ${userInputs[5]}
-    you have been chosen to be a ${userInputs[1]} My mission is to save the people of
-    ${userInputs[6]} by doing my favorite things: ${userInputs[7]},
-    ${userInputs[8]} and ${userInputs[9]}.
-    This may sound easy, but it is no walk in the park. It requires hard work and
-    ${userInputs[10]}. My weakness is eating ${userInputs[11]}.
-    They are gross!! Keep that away from me! I save the world every night. But when I wake up in the
-    morning, I go back to my normal life at ${userInputs[12]} School!`;
-
 // Declaring Total number of Questions and counter
 let numberOfQuestions = 12;
 let questionCounter = 0;
@@ -50,6 +36,22 @@ for (let i = numberOfQuestions; i >= 0; i--) {
 
 alert("All done! Ready for your totally-accurate, not-at-all confusing story about " + userInputs.slice(0, 1) + "?");
 
+let originalStory = `<h2>Hi! My name is ${userInputs[0]} and I have a secret to share
+    with you. I'm a normal child by day, and a ${userInputs[1]} by night.
+    Only you and my best friend ${userInputs[2]} knows my secret. You may be wondering
+    how this happened? Well, one night I was ${userInputs[3]}
+    at home, and then BOOM! The lights went out and ${userInputs[4]} showed up.
+    He/she said in a booming voice, because your favorite color is ${userInputs[5]}
+    you have been chosen to be a ${userInputs[1]} My mission is to save the people of
+    ${userInputs[6]} by doing my favorite things: ${userInputs[7]},
+    ${userInputs[8]} and ${userInputs[9]}.
+    This may sound easy, but it is no walk in the park. It requires hard work and
+    ${userInputs[10]}. My weakness is eating ${userInputs[11]}.
+    They are gross!! Keep that away from me! I save the world every night. But when I wake up in the
+    morning, I go back to my normal life at ${userInputs[12]} School!</h2>`;
+
+console.log(originalStory);
+document.write(originalStory);
 
 
 // Hi! My name is Sam and I have a secret to share
