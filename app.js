a6fc444d10f039e47d856f48b352ac0172c1a584
FarahSyed <farahsyed44@gmail.com>
2022-08-14 15:42:30 +0500

    Additional Polish

--- app.js
+++ app.js
@@ -34,21 +34,27 @@ for (let i = numberOfQuestions; i >= 0; i--) {
     numberOfQuestions--;
 }
 
-alert("All done! Ready for your totally-accurate, not-at-all confusing story about " + userInputs.slice(0, 1) + "?");
+for (let i = 0; i < questionArray.length; i++) {
+    if (userInputs[i] === "") {
+        userInputs[i] = "the user didn't write anything";
+    }
+}
 
-let originalStory = `<h2>Hi! My name is ${userInputs[0]} and I have a secret to share
-    with you. I'm a normal child by day, and a ${userInputs[1]} by night.
-    Only you and my best friend ${userInputs[2]} knows my secret. You may be wondering
-    how this happened? Well, one night I was ${userInputs[3]}
-    at home, and then BOOM! The lights went out and ${userInputs[4]} showed up.
-    He/she said in a booming voice, because your favorite color is ${userInputs[5]}
-    you have been chosen to be a ${userInputs[1]} My mission is to save the people of
-    ${userInputs[6]} by doing my favorite things: ${userInputs[7]},
-    ${userInputs[8]} and ${userInputs[9]}.
+alert("All done! Ready for your totally-accurate, not-at-all confusing story of " + userInputs.slice(0, 1) + "?");
+
+let originalStory = `<section><div><h1>All About Me</h1><h2>Hi! My name is <u>${userInputs[0]}</u> and I have a 
+    secret to share with you. I'm a normal child by day, and a <u>${userInputs[1]}</u> by night.
+    Only you and my best friend <u>${userInputs[2]}</u> knows my secret. You may be wondering
+    how this happened? Well, one night I was <u>${userInputs[3]}</u>
+    at home, and then BOOM! The lights went out and <u>${userInputs[4]}</u> showed up.
+    He/she said in a booming voice, because your favorite color is <u>${userInputs[5]}</u>
+    you have been chosen to be a <u>${userInputs[1]}</u> My mission is to save the people of
+    <u>${userInputs[6]}</u> by doing my favorite things: <u>${userInputs[7]}</u>,
+    <u>${userInputs[8]}</u> and <u>${userInputs[9]}</u>.
     This may sound easy, but it is no walk in the park. It requires hard work and
-    ${userInputs[10]}. My weakness is eating ${userInputs[11]}.
+    <u>${userInputs[10]}</u>. My weakness is eating <u>${userInputs[11]}</u>.
     They are gross!! Keep that away from me! I save the world every night. But when I wake up in the
-    morning, I go back to my normal life at ${userInputs[12]} School!</h2>`;
+    morning, I go back to my normal life at <u>${userInputs[12]}</u> School!</h2></div></section>`;
 
 console.log(originalStory);
 document.write(originalStory);
